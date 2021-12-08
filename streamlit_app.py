316874d384ae4e1bf540d636034d67a6590436f4
LarsKaleschke <lars@kaleschke.de>
2021-12-08 15:26:36 +0100

    1st try sea ice data

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Sea ice test')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATE_COLUMN = 'Date'
+
+DATA_URL = ('https://seaice.de/nh_awi_amsr2_regional_extent_area.csv')
+
+
 
 @st.cache
 def load_data(nrows):
@@ -17,20 +19,9 @@ def load_data(nrows):
     return data
 
 data_load_state = st.text('Loading data...')
-data = load_data(10000)
+data = load_data()
 data_load_state.text("Done! (using st.cache)")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+st.subheader('Raw data')
+st.write(data)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
